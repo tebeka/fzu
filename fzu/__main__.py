6b361e2474aa16d7f6a4e04b882dd7513739a2cd
Miki Tebeka <miki.tebeka@gmail.com>
2019-06-06 18:13:56 +0300

    project

--- fzu/__main__.py
+++ fzu/__main__.py
@@ -1,19 +1,33 @@
 #!/usr/bin/env python
-"""Fuzzy search unicode symbols"""
+"""Fuzzy search unicode symbols (requires fzf installed)"""
 
 from argparse import ArgumentParser
 from subprocess import run, PIPE
 from pathlib import Path
 
-parser = ArgumentParser(description=__doc__)
-args = parser.parse_args()
+__version__ = '0.1.0'
 
-symbols = Path(__file__).parent.absolute() / 'symbols'
-out = run(['fzf', '--preview', 'cat {}'], cwd=symbols, stdout=PIPE)
-if out.returncode != 0:
-    raise SystemExit('error: cannot find')
 
-sym_file = symbols / out.stdout.decode('utf-8').strip()
-with sym_file.open() as fp:
-    sym = fp.read()
-print(sym)
+def main():
+    parser = ArgumentParser(description=__doc__, prog='fzu')
+    parser.add_argument(
+        '--version', help='print version & exit', action='store_true',
+        default=False)
+    args = parser.parse_args()
+    if args.version:
+        print(__version__)
+        raise SystemExit()
+
+    symbols = Path(__file__).parent.absolute() / 'symbols'
+    out = run(['fzf', '--preview', 'cat {}'], cwd=symbols, stdout=PIPE)
+    if out.returncode != 0:
+        raise SystemExit()
+
+    sym_file = symbols / out.stdout.decode('utf-8').strip()
+    with sym_file.open() as fp:
+        sym = fp.read()
+    print(sym)
+
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,34 @@
+import re
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+def version():
+    pyfile = 'fzu/__main__.py'
+    with open(pyfile) as fp:
+        data = fp.read()
+
+    match = re.search(r"__version__ = '([^']+)'", data)
+    assert match, 'cannot find version in {}'.format(pyfile)
+    return match.group(1)
+
+
+setup(
+    name='fzu',
+    version=version(),
+    description='Fuzzy unicode search',
+    long_description=open('README.md').read(),
+    author='Miki Tebeka',
+    author_email='miki.tebeka@gmail.com',
+    license='BSD',
+    url='https://github.com/tebeka/fzu',
+    packages=['fzu'],
+    zip_safe=False,
+    entry_points={
+        'console_scripts': [
+            'fzu = fzu.__main__:main',
+        ]
+    },
+)
