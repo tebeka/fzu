2b36188c7b52046d25dea9b73b493a30d38bba35
Miki Tebeka <miki.tebeka@gmail.com>
2019-06-10 10:13:28 +0300

    start of tests

---
+++ tests/test_symbols.py
@@ -0,0 +1,13 @@
+import unicodedata
+
+import pytest
+
+from fzu.symbols import symbols
+
+
+@pytest.mark.parametrize('symbol', symbols)
+def test_symbol(symbol):
+    try:
+        unicodedata.lookup(symbol)
+    except KeyError:
+        assert False, symbol
